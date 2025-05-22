e01bd1be97c90674ff9cf3546af6f2f055c50358
joaosr <eng.jmsoares@gmail.com>
2025-05-22 14:19:04 +0200

    Added tests for product

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,7 +1,7 @@
 import pytest
 from sqlalchemy.ext.asyncio import AsyncSession
-from retrofun.models import Product
-from sqlalchemy import select
+from retrofun.models import Product, Manufacturer, Country
+from sqlalchemy import select, or_, func
 from retrofun.import_products import save_products
 
 
@@ -30,16 +30,23 @@ def products(session):
             "country": "USA",
         },
         {
+            "name": "Hobbit",
+            "manufacturer": "intercompex",
+            "year": 1990,
+            "cpu": "Z80 Compatible",
+            "country": "USSR",
+        },
+        {
             "name": "Atari 600XL",
             "manufacturer": "Atari, Inc.",
-            "year": 1983,
+            "year": 1986,
             "cpu": "6502",
             "country": "USA",
         },
         {
             "name": "Atari 800XL",
             "manufacturer": "Atari, Inc.",
-            "year": 1983,
+            "year": 1986,
             "cpu": "6502",
             "country": "USA",
         },
@@ -68,4 +75,63 @@ async def test_product_select_by_cpu(session: AsyncSession, products) -> None:
     expected = ["6502 CPU", "6502", "6502", "6502"]
     result = await session.scalars(q)
     result = result.all()
+    assert result == expected
+
+
+@pytest.mark.asyncio
+async def test_product_select_by_cpu_year(session: AsyncSession, products) -> None:
+    q = (
+        select(Product.name, Product.cpu, Product.year)
+        .where(
+            or_(Product.cpu.like("%Z80%"), Product.cpu.like("%6502%")),
+            Product.year < 1990,
+        )
+        .order_by(Product.name.asc())
+    )
+    expected = [
+        ("Apple IIe", "6502 CPU", 1983),
+        ("Aquarius", "Z80", 1983),
+        ("Atari 1200XL", "6502", 1983),
+        ("Atari 600XL", "6502", 1986),
+        ("Atari 800XL", "6502", 1986),
+    ]
+    result = await session.execute(q)
+    result = result.all()
+    assert result == expected
+
+
+@pytest.mark.asyncio
+async def test_product_select_cpu_by_year_period(
+    session: AsyncSession, products
+) -> None:
+    q = select(Product.cpu).where(Product.year.between(1980, 1989)).distinct()
+    result = await session.scalars(q)
+    result = result.all()
+    expected = ["6502", "6502 CPU", "Z80"]
+    assert result == expected
+
+
+@pytest.mark.asyncio
+async def test_manufacturer_select_by_name_initial_letter(
+    session: AsyncSession, products
+) -> None:
+    q = (
+        select(Manufacturer.name)
+        .where(Manufacturer.name.like("A%"))
+        .order_by(Manufacturer.name)
+        .distinct()
+    )
+    result = await session.scalars(q)
+    result = result.all()
+    expected = ["Apple Computer", "Atari, Inc."]
+    assert result == expected
+
+
+@pytest.mark.asyncio
+async def test_product_count_per_year(session: AsyncSession, products) -> None:
+    counter = func.count(Product.id).label(None)
+    q = select(Product.year, counter).group_by(Product.year).order_by(counter.desc())
+    result = await session.execute(q)
+    result = result.all()
+    expected = [(1983, 3), (1986, 2), (1990, 1)]
     assert result == expected
