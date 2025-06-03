692928210dfa33a04a95142ad89e133d6a8564e7
joaosr <eng.jmsoares@gmail.com>
2025-06-03 12:47:12 +0200

    Add VPC, Subnet and NACL resources

--- Dockerfile
+++ Dockerfile
@@ -7,10 +7,4 @@ RUN pip install -r requirements.txt
 
 COPY . .
 
-# RUN alembic upgrade head
-
-# RUN python retrofun/import_orders.py
-
-# RUN python retrofun/import_products.py
-
 CMD ["uvicorn", "retrofun.app:app", "--host", "0.0.0.0", "--port", "8000"]
--- terraform/main.tf
+++ terraform/main.tf
@@ -31,14 +31,34 @@ resource "aws_security_group" "app_sg" {
   }
 }
 
-resource "aws_instance" "fastapi_ec2" {
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "retrofun-vpc"
+  }
+}
+
+resource "aws_subnet" "retrofun_subnet" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "eu-central-1a"
+  tags = {
+    Name = "app-subnet"
+  }
+}
+
+resource "aws_instance" "retrofun_api_ec2" {
   ami           = "ami-03250b0e01c28d196"
   instance_type = var.instance_type
   key_name      = aws_key_pair.deployer_key.key_name
   vpc_security_group_ids = [aws_security_group.app_sg.id]
+  subnet_id = aws_subnet.retrofun_subnet.id
 
   tags = {
-    Name = "FastAPI-EC2"
+    Name = "Retrofun-API-EC2"
   }
 
   provisioner "remote-exec" {
@@ -56,5 +76,50 @@ resource "aws_instance" "fastapi_ec2" {
       private_key = file("~/.ssh/gitlab-ci")
       host        = self.public_ip
     }
+  }
+}
+
+resource "aws_network_acl" "retrofun_nacl" {
+  vpc_id = aws_subnet.retrofun_subnet.vpc_id
+  subnet_ids = [aws_subnet.retrofun_subnet.id]
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 8000
+    to_port    = 8000
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 200
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 1024
+    to_port    = 65535
+  }
+
+  egress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 65535
+  }
+
+  egress {
+    protocol   = "tcp"
+    rule_no    = 200
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 1024
+    to_port    = 65535
+  }
+
+  tags = {
+    Name = "retrofun-nacl"
   }
 }
