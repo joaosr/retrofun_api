f5c4c047d4d4622d44a4ec4b4d96d493f72fdb7a
joaosr <eng.jmsoares@gmail.com>
2025-11-28 20:25:14 +0100

    Move terraform

--- terraform/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "ec2_public_ip" {
-  description = "Public IP of the EC2 instance"
-  value       = aws_instance.retrofun_api_ec2.public_ip
-}
--- terraform/terraform.tfvars
+++
@@ -1,3 +0,0 @@
-region         = "eu-central-1"
-key_pair_name  = "gitlab-ci"
-public_key_path = "~/.ssh/gitlab-ci.pub"
